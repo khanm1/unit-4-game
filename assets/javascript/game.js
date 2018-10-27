846dcb6ec44f59a694bbb05d8ba98801724bb5de
mkhan <riponk9519@gamil.com>
2018-10-27 12:43:37 -0500

    Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
 
-	var compPick = Math.floor(Math.random() * 102) + 19; 
+	var compPick = Math.floor(Math.random() * 101) + 19; 
 		console.log("compPick: " + compPick); 
 		$(".randomNumber").html(compPick); 
 
